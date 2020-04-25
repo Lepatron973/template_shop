f127831a23bd0f1875a831b54644e4c1192ee8e3
Lepatron973 <joseph.jeremy@outlook.fr>
2020-04-26 00:43:03 +0200

    developpement de la page home

--- base.php
+++ base.php
@@ -3,18 +3,20 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="https://kit.fontawesome.com/8b165d18c8.js" crossorigin="anonymous"></script>
-    <a class="d-none" href="https://icons8.com/icon/83804/menu-carré">Menu carré icon by Icons8</a>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> 
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/home.css">
     <title>{{$title_site}}</title>
 </head>
 <body>
+    <a class="d-none" href="https://icons8.com/icon/83804/menu-carré">Menu carré icon by Icons8</a>
     <?php require_once './views/header.php' ?>
     <?php require_once './views/home.php' ?>
 
+    <!--script section-->
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="js/header.js"></script>
+    <script src="js/home.js"></script>
 </body>
 </html>
--- views/home.php
+++ views/home.php
@@ -1,5 +1,34 @@
 <div class="home">
-    <section class="slider">
+
+    <section id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+       <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+       </ol>
+       <div class="carousel-inner">
+           <div class="carousel-item active">
+               <img class="d-block w-100" src="https://source.unsplash.com/EbuaKnSm8Zw/800x533" alt="First slide">
+           </div>
+           <div class="carousel-item">
+               <img class="d-block w-100" src="https://source.unsplash.com/kG38b7CFzTY/800x533" alt="Second slide">
+           </div>
+           <div class="carousel-item">
+               <img class="d-block w-100" src="https://source.unsplash.com/nvzvOPQW0gc/800x533" alt="Third slide">
+           </div>
+       </div>
+           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="sr-only">Previous</span>
+           </a>
+           <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="sr-only">Next</span>
+           </a>
+    </section>
+    <section>
     salut
     </section>
+   
+
 </div>
